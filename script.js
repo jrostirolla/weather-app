a6a75252d54ecc56c83739e0362a25f160d57e30
James Rostirolla <j_rostirolla@hotmail.com>
2021-07-06 18:29:42 +1000

    todo list for JS added

---
+++ script.js
@@ -0,0 +1,12 @@
+//TODO: figure out the fetch mechanics
+    //TODO: Once I know this, inspect the data from the API and see how to use it
+
+//TODO: install moment.js parameters
+
+//TODO: write a "for" loop for the days of the week
+
+//TODO: autofill the form when search has been completed
+
+//TODO: create if statement to change text hightlight of UV text depending on levels
+
+//TODO: Create function to fill in form after selecting previous search item
